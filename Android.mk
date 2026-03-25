2b1653ac882fd1f282fbe1d4d00fa4f3589ec94f
Notganesh <chimeports@gmail.com>
2026-03-25 02:49:55 +0000

    lyriq: initial vendor blobs
    Signed-off-by: Notganesh <chimeports@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,23 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),lyriq)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,4547d34ef4e295fe7f5ebc15bdba496cbeded2b2)
+$(call add-radio-file-sha1-checked,radio/dtbo.img,d10bd7eea42498a1108e915699afa4ce85c252de)
+$(call add-radio-file-sha1-checked,radio/gz.img,07abeee28070b6bdedc80c75d375c26115679376)
+$(call add-radio-file-sha1-checked,radio/lk.img,ef271457b6115eb9c277788965730ac583edf88b)
+$(call add-radio-file-sha1-checked,radio/logo.img,092547772517610a3b9cbd1f2de52221771cd4c0)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,dbb5a25e747d152bf301c2ebde2b1428df4ef206)
+$(call add-radio-file-sha1-checked,radio/md1img.img,b2deea61383fd33591864a8a1d7fd321da3838b8)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,5c459ef02ed3eee7cac9a3186d5e6c561818f081)
+$(call add-radio-file-sha1-checked,radio/preloader.img,ad829ecc1b5858d78a3e76eb4b6350eddc0c5bf4)
+$(call add-radio-file-sha1-checked,radio/scp.img,d8e3c98226b81ba00d2ace059eb37d72b3d5021b)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,b340cc133f4f14f322e91e32d73aaad91f9bdb7c)
+$(call add-radio-file-sha1-checked,radio/sspm.img,8462f12934c58bb79b251951de4651ae62f3c62a)
+$(call add-radio-file-sha1-checked,radio/tee.img,67eb5e9e31c28953ee741afa75e9d9b6a667dc92)
+
+endif
---
+++ BoardConfigVendor.mk
@@ -0,0 +1,18 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    dpm \
+    dtbo \
+    gz \
+    lk \
+    logo \
+    mcupm \
+    md1img \
+    pi_img \
+    preloader \
+    scp \
+    spmfw \
+    sspm \
+    tee
